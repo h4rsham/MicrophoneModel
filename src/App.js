e144f143522cbd53021d8f107b2e85bd0c865b4c
h4rsham <harsham.latif@outlook.com>
2023-07-06 12:50:37 +0100

    Installed three.js packages and created a introductory animated 3d object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ThreeJsComponent from "./components/three";
+import Animate from "./components/cubeRender";
 
 function App() {
   return (
     <div className="App">
       <ThreeJsComponent />
+      <Animate />
     </div>
   );
 }
